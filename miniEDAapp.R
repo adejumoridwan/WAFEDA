7ef9e710dbcc711ac2994ffd5132e99e130125d0
adejumoridwan <adejumo999@gmail.com>
2022-03-02 15:39:00 -0800

    dd

--- miniEDAapp.R
+++ miniEDAapp.R
@@ -1,7 +1,11 @@
 library(shiny)
+library(tidyverse)
+library(rlang)
 
 not_sel = "Not Selected"
 
+#main page
+#----------------------------------------
 main_page <- tabPanel(
   title = "Analysis",
   titlePanel("Analysis"),
@@ -9,49 +13,215 @@ main_page <- tabPanel(
     sidebarPanel(
       title = "Inputs",
       fileInput("upload", NULL, accept = c(".csv", ".tsv")),
+      numericInput("n","Number of rows", value = 5),
       selectInput("num_var_1","Variable 1(numeric)",choices=c(not_sel)),
       selectInput("num_var_2","Variable 2(numeric)",choices=c(not_sel)),
       selectInput("fact_var","Variable 3(factor)",choices=c(not_sel)),
-      actionButton("run_button","Run Analysis",icon=icon("play"))
+      actionButton("run_button","Analyze",icon=icon("play"))
     ),
     mainPanel(
       tabsetPanel(
         tabPanel(
-          title = "Plot"
+          title = "Data",
+          tableOutput("head")
+        ),
+        tabPanel(
+          title = "Plot",
+          plotOutput("plot_1")
         ),
         tabPanel(
           title = "Statistics",
+          fluidRow(
+            column(width = 4, strong(textOutput("num_var_1_title"))),
+            column(width = 4, strong(textOutput("num_var_2_title"))),
+            column(width = 4, strong(textOutput("fact_var_title")))
+          ),
+          fluidRow(
+            column(width = 4, tableOutput("num_var_1_summary_table")),
+            column(width = 4, tableOutput("num_var_2_summary_table")),
+            column(width = 4, tableOutput("fact_var_summary_table"))
+          ),
+          fluidRow(
+            column(width = 12, strong("Combined Statistics"))
+          ),
+          fluidRow(
+            column(width = 12, tableOutput("combined_summary_table"))
+          )
         )
       )
     )
   )
 )
 
+#about page
+#-----------------------------------
 about_page <- tabPanel(
   title = "About", titlePanel("About"),
   "Created with R Shiny", br(), "2022 March")
 
+#function generating plots
+#-------------------------------------------------------
+draw_plot_1 <- function(data_input, num_var_1, num_var_2, fact_var){
+  if(fact_var!=not_sel){#convert non factor columns to factor
+    data_input[,(fact_var) := as.factor(data_input[,get(fact_var)])]
+  }
+  if(num_var_1 != not_sel & num_var_2 != not_sel & fact_var != not_sel){
+    ggplot(data = data_input,
+           aes_string(x = num_var_1, y = num_var_2, color = fact_var)) +
+      geom_point()
+  }
+  else if(num_var_1 != not_sel & num_var_2 != not_sel & fact_var == not_sel){
+    ggplot(data = data_input,
+           aes_string(x = num_var_1, y = num_var_2)) +
+      geom_point()
+  }
+  else if(num_var_1 != not_sel & num_var_2 == not_sel & fact_var != not_sel){
+    ggplot(data = data_input,
+           aes_string(x = fact_var, y = num_var_1)) +
+      geom_violin()
+  }
+  else if(num_var_1 == not_sel & num_var_2 != not_sel & fact_var != not_sel){
+    ggplot(data = data_input,
+           aes_string(x = fact_var, y = num_var_2)) +
+      geom_violin()
+  }
+  else if(num_var_1 != not_sel & num_var_2 == not_sel & fact_var == not_sel){
+    ggplot(data = data_input,
+           aes_string(x = num_var_1)) +
+      geom_histogram()
+  }
+  else if(num_var_1 == not_sel & num_var_2 != not_sel & fact_var == not_sel){
+    ggplot(data = data_input,
+           aes_string(x = num_var_2)) +
+      geom_histogram()
+  }
+  else if(num_var_1 == not_sel & num_var_2 == not_sel & fact_var != not_sel){
+    ggplot(data = data_input,
+           aes_string(x = fact_var)) +
+      geom_bar()
+  }
+}
+
+#function generating numeric variable statistics
+create_num_var_table <- function(data_input, num_var){
+  if(num_var != not_sel){
+    col <- data_input[,get(num_var)]
+    if (length(col)>5000) col_norm <- sample(col,5000) else col_norm <- col
+    norm_test <- shapiro.test(col_norm)
+    statistic <- c("mean", "median", "5th percentile", "95th percentile",
+                   "Shapiro statistic", "Shapiro p-value")
+    value <- c(round(mean(col),2), round(median(col),2),
+               round(quantile(col, 0.05),2), round(quantile(col, 0.95),2),
+               norm_test$statistic, norm_test$p.value)
+    data.table(statistic, value)
+  }
+}
+
+#function generating factor variable statistics
+create_fact_var_table <- function(data_input, fact_var){
+  if(fact_var != not_sel){
+    freq_tbl <- data_input[,.N, by = get(fact_var)]
+    freq_tbl <- setnames(freq_tbl,c("factor_value", "count"))
+    freq_tbl
+  }
+}
+
+#function generating combined table statistics
+create_combined_table <- function(data_input, num_var_1, num_var_2, fact_var){
+  if(fact_var != not_sel){
+    if(num_var_1 != not_sel & num_var_2 != not_sel){
+      res_tbl <- data_input[,.(correlation = cor(get(num_var_1), get(num_var_2))), by = fact_var]
+    }
+    else if(num_var_1 != not_sel & num_var_2 == not_sel){
+      res_tbl <- data_input[,.(mean = mean(get(num_var_1))), by = fact_var]
+    }
+    else if(num_var_1 == not_sel & num_var_2 != not_sel){
+      res_tbl <- data_input[,.(mean = mean(get(num_var_2))), by = fact_var]
+    }
+  }
+  else if(num_var_1 != not_sel & num_var_2 != not_sel){
+    res_tbl <- data.table(
+      statistic = c("correlation"),
+      value = c(cor(
+        data_input[,get(num_var_1)],
+        data_input[,get(num_var_2)])))
+  }
+  return(res_tbl)
+}
+
+#front end
+#----------------------------------------
 ui <- 
   navbarPage(
   title = "mini EDA", 
   main_page, 
   about_page)
 
+#back_end
+#---------------------------------------------
 server <- function(input, output, session) {
   data <- reactive({
     req(input$upload)
-    
-    ext <- tools::file_ext(input$upload$name)
-    switch(ext,
-           csv = vroom::vroom(input$upload$datapath, delim = ","),
-           tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
-           validate("Invalid file; Please upload a .csv or .tsv file")
-    )
+    fread(input$upload$datapath)
   })
   
+  #number of rows
   output$head <- renderTable({
     head(data(), input$n)
   })
+  
+  observeEvent(data(),{
+    choices <- c(not_sel,names(data()))
+    updateSelectInput(inputId = "num_var_1", choices = choices)
+    updateSelectInput(inputId = "num_var_2", choices = choices)
+    updateSelectInput(inputId = "fact_var", choices = choices)
+  })
+  
+  num_var_1 <- eventReactive(input$run_button,input$num_var_1)
+  num_var_2 <- eventReactive(input$run_button,input$num_var_2)
+  fact_var <- eventReactive(input$run_button,input$fact_var)
+  
+  plot_1 <- eventReactive(input$run_button,{
+    draw_plot_1(data(), num_var_1(), num_var_2(), fact_var())
+  })
+  
+  output$plot_1 <- renderPlot(plot_1())
+  
+  output$num_var_1_title <- 
+    renderText(paste("Num Var 1:",num_var_1()))
+  
+  num_var_1_summary_table <- eventReactive(input$run_button,{
+    create_num_var_table(data(), num_var_1())
+  })
+  
+  output$num_var_1_summary_table <- 
+    renderTable(num_var_1_summary_table(),colnames = FALSE)
+  
+  output$num_var_2_title <- 
+    renderText(paste("Num Var 2:",num_var_2()))
+  
+  num_var_2_summary_table <- eventReactive(input$run_button,{
+    create_num_var_table(data(), num_var_2())
+  })
+  
+  output$num_var_2_summary_table <- 
+    renderTable(num_var_2_summary_table(),colnames = FALSE)
+  
+  output$fact_var_title <- 
+    renderText(paste("Factor Var:",fact_var()))
+  
+  fact_var_summary_table <- eventReactive(input$run_button,{
+    create_fact_var_table(data(), fact_var())
+  })
+  
+  output$fact_var_summary_table <- 
+    renderTable(fact_var_summary_table(), colnames = FALSE)
+  
+  combined_summary_table <- eventReactive(input$run_button,{
+    create_combined_table(data(), num_var_1(), num_var_2(), fact_var())
+  })
+  
+  output$combined_summary_table <- renderTable(combined_summary_table())
 }
 
 shinyApp(ui, server)
